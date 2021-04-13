325c2a58210c4423a1f476a778ba858c4bf4f39f
jinamshah <jinam.shah@cactusglobal.com>
2021-04-13 23:55:47 +0530

    changed repo name in bash script

--- emr/get_code.sh
+++ emr/get_code.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-#cd /home/hadoop/
+
 sudo -u root bash -c 'chown -R hadoop /home/hadoop/'
 
 mkdir -p /home/hadoop/blog_code/
 cd /home/hadoop/blog_code/ && git init
 
-git pull https://jinamshah/blog.git;
+git pull https://jinamshah/aws-data-ops-article-1.git;
 git checkout master
 
-#export PYTHONPATH=$PYTHONPATH:/home/hadoop/blog_code/
+# we can also add python requirements here. for example:
+# python3 -m pip install pyspark
